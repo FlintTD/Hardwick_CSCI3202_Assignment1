92f22962e5a70a8d4268a8eba31674c9f3dd3c0f
FlintTD <erha8359@colorado.edu>
2015-09-04 15:54:37 -0600

    all structures implemented; methods incomplete; code documented

--- Hardwick_Assignment1.py
+++ Hardwick_Assignment1.py
@@ -1,13 +1,24 @@
 import Queue
 
 
-class Queue(object):
-    def add(self, data):                    # simple overlay function that prevents non-integer storage
-        if data.type() is int:
-            self.put(data)
+class SafeQueue(object):
+    safe = Queue.Queue()
+
+    def safe_put(self, data):                    # simple overlay function that prevents non-integer storage
+        if type(data) is int:
+            self.safe.put(data)
             print "Integer stored!"
         else:
             print "This is not an integer."
+
+    def safe_get(self):
+        return self.safe.get()
+
+    def qsize(self):
+        return self.safe.qsize()
+
+    def empty(self):
+        return self.safe.empty()
 
 
 class Stack(object):
@@ -15,8 +26,8 @@ class Stack(object):
     count = 0
 
     def push(self, data):
-        if data.type() is int:
-            self.stack + [data]
+        if type(data) is int:
+            self.stack = self.stack + [data]
             self.count += 1
             print "Integer stored!"
         else:
@@ -24,13 +35,15 @@ class Stack(object):
 
     def pop(self):
         if self.count > 0:
-            del self.list[self.count - 1]
+            ret = self.stack[(self.count - 1)]
+            del self.stack[(self.count - 1)]
             self.count -= 1
             print "Stack top popped!"
+            return ret
         else:
             print "Stack is empty, cannot pop."
 
-    def checkSize(self):
+    def check_size(self):
         return self.count
 
 
@@ -44,14 +57,17 @@ class BinaryNode(object):
         self.key = value
         self.parent = parent
 
-    def search(self, value):                # self-searching nodes are convenient; search invoked via the tree
-        if self.key == value:
+    def search(self, parentValue):                # self-searching nodes are convenient; search invoked via the tree
+        print "%r %r" % (self.key, parentValue)
+        if self.key == parentValue:
             return self
-        elif (self.key != value) and (self.left is not None):
-            self.search(self.left, value)
-        elif (self.key != value) and (self.right is not None):
-            self.search(self.right, value)
-        return None
+        elif (self.key != parentValue) and (self.left is not None):
+            return self.left.search(parentValue)
+        elif (self.key != parentValue) and (self.right is not None):
+            return self.right.search(parentValue)
+        else:
+            print "None"
+            return None
 
     def cull(self, value):                  # self-deleting nodes are also convenient; delete invoked via the tree
         if self.left is not None:
@@ -70,19 +86,24 @@ class BinaryTree(object):                   # tree starts empty, root node added
     root = None
 
     def add(self, value, parentValue):
-        if self.root is None and parentValue is None:
+        if (self.root is None) and (parentValue is None):
             self.root = BinaryNode(value, None)
+            return True
         else:
-            solution = self.root.search(value)
+            solution = self.root.search(parentValue)
             if solution is not None:
                 if solution.left is None:
-                    solution.left = BinaryNode(value, solution.left)
+                    solution.left = BinaryNode(value, parentValue)
+                    return True
                 elif solution.right is None:
-                    solution.right = BinaryNode(value, solution.right)
+                    solution.right = BinaryNode(value, parentValue)
+                    return True
                 else:
                     print "Parent has two children, node not added."
+                    return False
             else:
                 print "Parent not found."
+                return False
 
     def delete(self, value):                # invokes self-deleting process, after ensuring there is a node to delete
         if self.root is None:
@@ -138,16 +159,59 @@ class Graph(object):
                 print self.graph[value]
 
 
+
 # tests exist below this line
 
-print "! ! Beginning Queue Tests ! !"       # Queue Tests
+# Queue Tests
+print "! ! Beginning Queue Tests ! !"
+TestQueue = SafeQueue()
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]:  # ints to queue
+    TestQueue.safe_put(x)                        # Adding ints to queue
+    print "Added %d. " % x
+print "Size of queue is now %d. " % TestQueue.qsize()
+while TestQueue.empty() is not True:        # Removing queued ints
+    z = TestQueue.safe_get()
+    print "Removed %d." % z
+if TestQueue.empty():
+    print "Queue is now empty."             # Double-check of queue emptiness
+else:
+    print "Queue remains un-empty."
+print "! ! Ending Queue Tests ! !"
 
 
-print "! ! Beginning Stack Tests ! !"       # Stack Tests
+# Stack Tests
+print "! ! Beginning Stack Tests ! !"
+TestStack = Stack()
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]:  # ints to stack
+    TestStack.push(x)                       # Adding ints to stack
+    print "Added %d. " % x
+print "Size of stack is now %d. " % TestStack.check_size()
+while TestStack.check_size() > 0:           # Removing stacked ints
+    y = TestStack.pop()
+    print "Popped %d." % y
+if TestStack.check_size() == 0:
+    print "Stack is now empty."             # Double-check of stack emptiness
+else:
+    print "Stack remains un-empty."
+print "! ! Ending Stack Tests ! !"
 
 
-print "! ! Beginning Tree Tests  ! !"       # Tree Tests
+# Tree Tests
+print "! ! Beginning Tree Tests ! !"
+TestTree = BinaryTree()
+parentList = {1:None, 2:1, 3:1, 4:2, 5:2, 6:3, 7:3, 8:4, 9:4, 10:5}      # parents to store values under
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:     # values to be stored
+    c = TestTree.add(x, parentList[x])    # adding nodes to the tree
+    if c:
+        print "Added %d to %r." % (x, parentList[x])
+    else:
+        print "Failed to add %d to %r." % (x, parentList[x])
+print "Now printing Binary Tree below:"
+TestTree.print_tree()
+print "! ! Ending Tree Tests ! !"
 
 
-print "! ! Beginning Graph Tests ! !"       # Graph Tests
+# Graph Tests
+print "! ! Beginning Graph Tests ! !"
+print "! ! Ending Graph Tests ! !"
 
